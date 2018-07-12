3e183750f2d8a054c8ed01afbdb0f8275c02c4a6
Vaclav Blazek <vaclav.blazek@gmail.com>
2018-07-12 10:31:06 +0200

    added local vts debian repo

--- bin/install_vts.sh
+++ bin/install_vts.sh
@@ -24,57 +24,57 @@ BUILD_DIR=`pwd`
 
 apt-get -q update
 
-# #Install packages
-# apt-get --assume-yes install dpkg-dev
+#Install packages
+apt-get --assume-yes install dpkg-dev
 
-# WORKDIR=/tmp/melown-bionic
-# REPODIR=/usr/local/share/melown-bionic
+WORKDIR=/tmp/melown-bionic
+REPODIR=/usr/local/share/melown-bionic
 
-# # fetch packages
-# mkdir -p ${WORKDIR}
-# (
-#     cd ${WORKDIR}
-#     wget --accept-regex "\.deb$" --recursive -l 1 -nd -np \
-#          http://cdn.melown.com/packages/repos/melown-bionic/
+# fetch packages
+mkdir -p ${WORKDIR}
+(
+    cd ${WORKDIR}
+    wget --accept-regex "\.deb$" --recursive -l 1 -nd -np \
+         http://cdn.melown.com/packages/repos/melown-bionic/
 
-#     files=$(ls -1 *.deb | grep -v -- "-dbg" | sort -V | \
-#                    awk 'BEGIN {FS="_"} /\.deb$/ {packages[$1]=$0} END {for (package in packages) { print packages[package] }}')
+    files=$(ls -1 *.deb | grep -v -- "-dbg" | sort -V | \
+                   awk 'BEGIN {FS="_"} /\.deb$/ {packages[$1]=$0} END {for (package in packages) { print packages[package] }}')
 
 
-#     # build repository
-#     mkdir -p ${REPODIR}
-#     mv ${files} ${REPODIR}
-#     cd ${REPODIR}
-#     dpkg-scanpackages . /dev/null > Packages
-#     gzip --keep --force -9 Packages
+    # build repository
+    mkdir -p ${REPODIR}
+    mv ${files} ${REPODIR}
+    cd ${REPODIR}
+    dpkg-scanpackages . /dev/null > Packages
+    gzip --keep --force -9 Packages
 
-#     # build a release file
-#     cat > Release <<EOF
-# Origin: Melown Technologies SE
-# Label: Melown Technologies SE
-# Suite: melown-bionic-local
-# Codename: melown-bionic-local
-# Architectures: all i386 amd64
-# EOF
+    # build a release file
+    cat > Release <<EOF
+Origin: Melown Technologies SE
+Label: Melown Technologies SE
+Suite: melown-bionic-local
+Codename: melown-bionic-local
+Architectures: all i386 amd64
+EOF
 
-#     echo -e "Date: `LANG=C date -Ru`" >> Release
+    echo -e "Date: `LANG=C date -Ru`" >> Release
 
-#     # Release must contain MD5 sums of all repository files (in a simple repo
-#     # just the Packages and Packages.gz file)s
-#     echo -e 'MD5Sum:' >> Release
-#     printf ' '$(md5sum Packages.gz | cut --delimiter=' ' --fields=1)' %16d Packages.gz' $(wc --bytes Packages.gz | cut --delimiter=' ' --fields=1) >> Release
-#     printf '\n '$(md5sum Packages | cut --delimiter=' ' --fields=1)' %16d Packages' $(wc --bytes Packages | cut --delimiter=' ' --fields=1) >> Release
+    # Release must contain MD5 sums of all repository files (in a simple repo
+    # just the Packages and Packages.gz file)s
+    echo -e 'MD5Sum:' >> Release
+    printf ' '$(md5sum Packages.gz | cut --delimiter=' ' --fields=1)' %16d Packages.gz' $(wc --bytes Packages.gz | cut --delimiter=' ' --fields=1) >> Release
+    printf '\n '$(md5sum Packages | cut --delimiter=' ' --fields=1)' %16d Packages' $(wc --bytes Packages | cut --delimiter=' ' --fields=1) >> Release
 
-#     # Release must contain SHA256 sums of all repository files (in a simple repo
-#     # just the Packages and Packages.gz files)
-#     echo -e '\nSHA256:' >> Release
-#     printf ' '$(sha256sum Packages.gz | cut --delimiter=' ' --fields=1)' %16d Packages.gz' $(wc --bytes Packages.gz | cut --delimiter=' ' --fields=1) >> Release
-#     printf '\n '$(sha256sum Packages | cut --delimiter=' ' --fields=1)' %16d Packages' $(wc --bytes Packages | cut --delimiter=' ' --fields=1) >> Release
+    # Release must contain SHA256 sums of all repository files (in a simple repo
+    # just the Packages and Packages.gz files)
+    echo -e '\nSHA256:' >> Release
+    printf ' '$(sha256sum Packages.gz | cut --delimiter=' ' --fields=1)' %16d Packages.gz' $(wc --bytes Packages.gz | cut --delimiter=' ' --fields=1) >> Release
+    printf '\n '$(sha256sum Packages | cut --delimiter=' ' --fields=1)' %16d Packages' $(wc --bytes Packages | cut --delimiter=' ' --fields=1) >> Release
 
-#     echo "deb [trusted=yes] file:${REPODIR} ./" \
-#          > /etc/apt/sources.list.d/melown-bionic-local.list
-# )
-# rm -rf ${WORKDIR}
+    echo "deb [trusted=yes] file:${REPODIR} ./" \
+         > /etc/apt/sources.list.d/melown-bionic-local.list
+)
+rm -rf ${WORKDIR}
 
 
 VTS_PACKAGES="vts-registry vts-tools vts-vtsd vts-mapproxy vts-mapproxy-tools vts-backend"
